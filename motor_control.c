cb04af9621a8fef6969d5d1a20e694ef638c80f0
Apoorva_git <121378763+ApoorvaGithubAmargol@users.noreply.github.com>
2024-11-13 19:44:36 +0530

    Create motor_control.c

---
+++ motor_control.c
@@ -0,0 +1,14 @@
+// we control the motor via motor driver
+int motor1pin1=13;          //declaring user defined variable motor1pin1 with the pin numbers of esp32
+int motore1pin2=12;
+
+void setup()
+{
+  pinMode(motor1pin1,OUTPUT); //defining the dclared pins as output
+  pinMode(motor1pin2,OUTPUT);
+}
+void loop()
+{
+  digitalWrite(motor1pin1,HIGH);    //writing high to low, making motor turn clock wise
+  digitalWrite(motor1pin1,HIGH);
+}
